40f860779998b0d4a4079b50f5c90a461556920e
Bincy Mathew <bincy4395@gmail.com>
2020-11-18 11:29:58 +0400

    html changes

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+
+class FilesController extends Controller
+{
+    public function search(Request $request)
+    {      
+        $path='public/directories';
+        $keyword = $request->get('search_keyword');
+        $allFiles = [];
+
+        //check for extensions
+        $extension =  strstr($keyword,".");
+       
+
+        $sub_folder = $path .'/' . $keyword;
+        $allFiles = Storage::allFiles( $sub_folder);
+        
+        return view('search')->with('files', $allFiles); 
+              
+        
+      
+    }
+}
